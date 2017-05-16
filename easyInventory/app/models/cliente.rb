93ac95c065b06b20c40f0d081045b6e30a31f706
patricio <patriciocabreravera@gmail.com>
2017-05-16 10:52:43 -0400

    Se agregaron asociaciones y validaciones

--- easyInventory/app/models/cliente.rb
+++ easyInventory/app/models/cliente.rb
@@ -1,2 +1,5 @@
 class Cliente < ApplicationRecord
+  has_many :producto
+  validates :nombre, presence: true,
+                    length: { minimum: 20 }
 end
--- easyInventory/app/models/movimiento.rb
+++ easyInventory/app/models/movimiento.rb
@@ -1,2 +1,4 @@
 class Movimiento < ApplicationRecord
+  has_many :producto
+  validates :fecha, presence: true
 end
